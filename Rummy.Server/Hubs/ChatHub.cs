d67dd42b05b1fef668bcb2ca6018ac0950f15867
Dumitru-Daniel Mititelu <mttl_daniel@yahoo.com>
2019-01-05 21:17:41 +0200

    Add chat rooms

--- Rummy.Server/Hubs/ChatHub.cs
+++ Rummy.Server/Hubs/ChatHub.cs
@@ -1,10 +1,39 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Rummy.Server.Hubs
 {
     public class ChatHub : Hub
     {
+        private List<string> rooms;
+
+        public ChatHub()
+        {
+            rooms = new List<string>();
+        }
+
+        public async void CreateRoom(string roomName)
+        {
+            rooms.Add(roomName);
+            await JoinRoom(roomName);
+        }
+
+        public async Task JoinRoom(string roomName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, roomName);
+        }
+
+        public async Task LeaveRoom(string roomName)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, roomName);
+        }
+
+        public async Task SendMessageToRoom(string roomName, string user, string message)
+        {
+            await Clients.Group(roomName).SendAsync("ReceiveMessage", user, message);
+        }
+
         public async Task SendMessage(string user, string message)
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);

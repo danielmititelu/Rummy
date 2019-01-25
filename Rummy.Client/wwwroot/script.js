e21d23a09bb9ec01f577aa185daee9bb371dfb75
Dumitru-Daniel Mititelu <mttl_daniel@yahoo.com>
2019-01-25 18:31:28 +0200

    Add draggable library

--- Rummy.Client/wwwroot/script.js
+++ Rummy.Client/wwwroot/script.js
@@ -1,47 +1,11 @@
-﻿window.dragElement = (elmnt) => {
-    //var elmnt = document.getElementById('myCanvas');
-    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-
-    /* otherwise, move the DIV from anywhere inside the DIV:*/
-    elmnt.onmousedown = dragMouseDown;
-
-    function dragMouseDown(e) {
-        e = e || window.event;
-        e.preventDefault();
-        // get the mouse cursor position at startup:
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        document.onmouseup = closeDragElement;
-        // call a function whenever the cursor moves:
-        document.onmousemove = elementDrag;
-    }
-
-    function elementDrag(e) {
-        e = e || window.event;
-        e.preventDefault();
-        // calculate the new cursor position:
-        pos1 = pos3 - e.clientX;
-        pos2 = pos4 - e.clientY;
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        // set the element's new position:
-        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-    }
-
-    function closeDragElement() {
-        /* stop moving when mouse button is released:*/
-        document.onmouseup = null;
-        document.onmousemove = null;
-    }
-    return true;
-};
+﻿//import { Droppable } from '@shopify/draggable';
 
 window.cropImage = (i, j) => {
     var tileWidth = 32;
     var tileHeight = 48;
+
     var canvas = document.createElement("canvas");
-    //canvas.setAttribute('id', "myCanvas");
+    canvas.setAttribute('class', "piece");
     canvas.setAttribute('width', tileWidth);
     canvas.setAttribute('height', tileHeight);
     var context = canvas.getContext('2d');
@@ -56,8 +20,29 @@ window.cropImage = (i, j) => {
 
         context.drawImage(imageObj, sourceX, sourceY, tileWidth, tileHeight, destX, destY, tileWidth, tileHeight);
     };
+
     imageObj.src = 'images/Tiles.png';
-    $('#board').append(canvas);
-    dragElement(canvas);
+    var dropzone = $('.dropzone:first');
+    dropzone.append(canvas);
+    dropzone.addClass('draggable-dropzone--occupied');
+
+    var options = {
+        draggable: '.piece',
+        dropzone: '.dropzone'
+    };
+    var droppable = new Draggable.Droppable(document.querySelectorAll('.dropzone'), options);
+
+    //var droppableOrigin;
+    
+    //droppable.on('drag:start', (evt) => {
+    //    droppableOrigin = evt.originalSource.parentNode.dataset.dropzone;
+    //});
+
+    //droppable.on('droppable:dropped', (evt) => {
+    //    if (droppableOrigin !== evt.dropzone.dataset.dropzone) {
+    //        evt.cancel();
+    //    }
+    //});
+    
     return true;
 };

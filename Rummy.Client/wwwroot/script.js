ebf7ee03fd1904df86f3cde6ec952cefbe3a9486
Dumitru-Daniel Mititelu <mttl_daniel@yahoo.com>
2019-01-27 19:59:55 +0200

    Add snappable

--- Rummy.Client/wwwroot/script.js
+++ Rummy.Client/wwwroot/script.js
@@ -1,48 +1,10 @@
-﻿//import { Droppable } from '@shopify/draggable';
+﻿window.cropImage = (i, j) => {
+    const containers = document.querySelectorAll('.swappable-container');
 
-window.cropImage = (i, j) => {
-    var tileWidth = 32;
-    var tileHeight = 48;
-
-    var canvas = document.createElement("canvas");
-    canvas.setAttribute('class', "piece");
-    canvas.setAttribute('width', tileWidth);
-    canvas.setAttribute('height', tileHeight);
-    var context = canvas.getContext('2d');
-    var imageObj = new Image();
-
-    imageObj.onload = function () {
-        // draw cropped image
-        var sourceX = j * tileWidth;
-        var sourceY = i * tileHeight;
-        var destX = 0;
-        var destY = 0;
-
-        context.drawImage(imageObj, sourceX, sourceY, tileWidth, tileHeight, destX, destY, tileWidth, tileHeight);
-    };
-
-    imageObj.src = 'images/Tiles.png';
-    var dropzone = $('.dropzone:first');
-    dropzone.append(canvas);
-    dropzone.addClass('draggable-dropzone--occupied');
-
-    var options = {
-        draggable: '.piece',
-        dropzone: '.dropzone'
-    };
-    var droppable = new Draggable.Droppable(document.querySelectorAll('.dropzone'), options);
-
-    //var droppableOrigin;
-    
-    //droppable.on('drag:start', (evt) => {
-    //    droppableOrigin = evt.originalSource.parentNode.dataset.dropzone;
-    //});
-
-    //droppable.on('droppable:dropped', (evt) => {
-    //    if (droppableOrigin !== evt.dropzone.dataset.dropzone) {
-    //        evt.cancel();
-    //    }
-    //});
+    const swappable = new Draggable.Swappable(containers, {
+        draggable: '.cell',
+        plugins: [Draggable.Plugins.Snappable]
+    });
     
     return true;
 };

366fc59b599cffe098e73eb6f215fcfa4c2dc671
Dumitru-Daniel Mititelu <mttl_daniel@yahoo.com>
2019-06-15 13:22:52 +0300

    Migrate to latest blazor version
    Add project with latest blazor version and server-side rendering

---
+++ OldProject/Rummy.Client/wwwroot/script.js
@@ -0,0 +1,18 @@
+﻿window.mountDraggable = (i, j) => {
+    const containers = document.querySelectorAll('.swappable-container');
+
+    const swappable = new Draggable.Swappable(containers, {
+        draggable: '.cell',
+        plugins: [Draggable.Plugins.Snappable]
+    });
+
+    swappable.on('drag:start', (evt) => {
+        pos = {
+            x: evt.sensorEvent.clientX,
+            y: evt.sensorEvent.clientY
+        };
+        console.log('drag start:', pos.x, pos.y);
+    });
+    
+    return true;
+};

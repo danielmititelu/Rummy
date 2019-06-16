c6e773cd89f61fd89fec5ea127655bde736925e0
Dumitru-Daniel Mititelu <mttl_daniel@yahoo.com>
2019-06-16 16:15:57 +0300

    Add test pieces to table

---
+++ Rummy/Models/PieceModel.cs
@@ -0,0 +1,23 @@
+﻿namespace Rummy.Models
+{
+    public class PieceModel
+    {
+        public enum Colors
+        {
+            Black,
+            Red,
+            Blue,
+            Yellow,
+            Joker
+        }
+
+        public int Number { get; set; }
+        public Colors Color { get; set; }
+
+        public PieceModel(int number, Colors color)
+        {
+            Number = number;
+            Color = color;
+        }
+    }
+}
